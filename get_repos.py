57904a1c8c86362bce869d7e2ed87b2e36846004
loyvon <buccaneerhero@163.com>
2023-12-16 02:09:13 +0800

    Added nlp2sql functionality

--- get_repos.py
+++ get_repos.py
@@ -1,29 +1,35 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import time
+from config import config
 
-username = "loyvon"
-token = "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"
-headers = {'Accept': 'application/vnd.github+json'}
+def get_repos():
+    username = config['username']
+    token = config['token']
+    headers = {'Accept': 'application/vnd.github+json'}
 
-topics = ["database", "big-data", "data-analytics", "data-visualization", "programming-language", "distributed-system",
-          "artificial-intelligence", "machine-learning", "deep-learning"]
+    topics = ["database", "big-data", "data-analytics", "data-visualization", "programming-language", "distributed-system",
+              "artificial-intelligence", "machine-learning", "deep-learning"]
 
-for topic in topics:
-    page_id = 1
-    while True:
-        url = ("https://api.github.com/search/repositories?q={}+in%3Atopics&sort=stars&order=desc&page={}&per_page=100"
-            .format(topic, page_id))
-        response = requests.get(url, auth=HTTPBasicAuth(username, token), headers=headers)
-        if not response.ok:
-            print(response.text)
-            break
-        with open('data/raw/{}_page_{:08d}.json'.format(topic, page_id), 'w') as f:
-            f.write(response.text)
-            f.write('\n')
-            f.flush()
-            print("Dumped page {} {}".format(topic, page_id))
-        page_id += 1
-        time.sleep(2)
+    for topic in topics:
+        page_id = 1
+        while True:
+            url = ("https://api.github.com/search/repositories?q={}+in%3Atopics&sort=stars&order=desc&page={}&per_page=100"
+                .format(topic, page_id))
+            response = requests.get(url, auth=HTTPBasicAuth(username, token), headers=headers)
+            if not response.ok:
+                print(response.text)
+                break
+            with open('{}/raw/{}_page_{:08d}.json'.format(config['data_dir'], topic, page_id), 'w') as f:
+                f.write(response.text)
+                f.write('\n')
+                f.flush()
+                print("Dumped page {} {}".format(topic, page_id))
+            page_id += 1
+            time.sleep(2)
 
-print("Finished dumping pages")
+    print("Finished dumping pages")
+
+
+if __name__ == "__main__":
+    get_repos()
---
+++ nlp2sql.py
@@ -0,0 +1,61 @@
+import os
+import re
+
+from config import config
+from openai import AzureOpenAI, OpenAI
+import sqlite3
+
+if config['openai_api_type'] == "azure":
+    client = AzureOpenAI(api_version=config["openai_api_version"],
+                         api_key=config["openai_api_key"].strip(),
+                         azure_endpoint=config["openai_azure_endpoint"].strip())
+else:
+    client = OpenAI()
+
+
+def load_tables_schema(conn):
+    """Load the table schema as return the schema in text format."""
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = c.fetchall()
+    table_schemas = []
+    for table in tables:
+        c.execute(f"PRAGMA table_info({table[0]})")
+        schema = c.fetchall()
+        table_schemas.append(f"Schema for {table[0]}: {','.join([_[1] for _ in schema])}")
+    return ';'.join(table_schemas)
+
+
+def question2sql(conn, question):
+    prompt = ("SQLite3 SQL tables, with their schemas:\n\n{}"
+              "\n\nA query to answer question: ```{}```\n\n"
+              "Query: ".format(load_tables_schema(conn), question))
+    print(prompt)
+    response = client.chat.completions.create(model=config["openai_azure_deployment"],
+                                              messages=[{"role": "system",
+                                                         "content": "You are a database expert that helps people generate queries for their questions "
+                                                                    "based on given table schemas."},
+                                                        {"role": "user", "content": prompt}],
+                                              temperature=0,
+                                              max_tokens=150,
+                                              top_p=1,
+                                              frequency_penalty=0,
+                                              presence_penalty=0,
+                                              stop=["#", ";"])
+    msg = response.choices[0].message.content
+    if msg.startswith("```sql"):
+        return msg.strip("```sql")
+    return None
+
+
+if __name__ == "__main__":
+    DB_PATH = os.path.join(config["data_dir"], "repos.db")
+    conn = sqlite3.connect(DB_PATH)
+    while True:
+        user_input = input("Please enter your question (or 'quit' to stop): ")
+        if user_input.lower() == 'quit':
+            break
+        else:
+            print(f"You entered: {user_input}")
+            print(f"Generated sql: {question2sql(conn, user_input)}")
+    conn.close()
