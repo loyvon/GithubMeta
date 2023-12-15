1d497dc451029a5cfe8da09c8981a18fdaf5ccf2
loyvon <buccaneerhero@163.com>
2023-12-16 01:19:24 +0800

    Reformat server.py

--- server.py
+++ server.py
@@ -3,11 +3,13 @@ import threading
 
 app = Flask(__name__)
 
-LOCAL_PATH = 'data' # Change this to your path
+LOCAL_PATH = 'data'  # Change this to your path
+
 
 @app.route("/")
 def home():
     return "Web server is running"
+
 
 if __name__ == "__main__":
     app.run(port=5000)
