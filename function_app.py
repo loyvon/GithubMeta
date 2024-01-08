78c90cb2e29b5f0ac3af2775bca4622c9520bfd3
loyvon <buccaneerhero@163.com>
2024-01-08 20:30:03 +0800

    WIP

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ import utils
 app = func.FunctionApp()
 
 
-@app.timer_trigger(schedule="0 0 0 MON * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
+@app.timer_trigger(schedule="0 0 0 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def LoadRepos(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
